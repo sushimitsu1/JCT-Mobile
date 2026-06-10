e736eccf7445f16b17ee4e8f839d957769c2ebcd
JCT <sushimitsu1@gmail.com>
2026-06-10 11:57:29 -0700

    Custom domain mobile.jctlogisticsinc.com

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/JCT-Mobile/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
